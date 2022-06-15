49040d3d64dc5e68f88e785173be042fd285c363
Bob Nearents <bobnearents@gmail.com>
2022-06-15 15:42:14 -0700

    add cors

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const request = require("request");
 const app = express();
 app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.get("/", function (req, res) {
   res.send("hello world");
 });
