eb90c4a8605e270f0f219d927d2715822f3b9d21
Bob Nearents <bobnearents@gmail.com>
2022-06-16 17:24:11 -0700

    new headers

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://bobnearents.com"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
+    "Access-Control-Request-Private-Network",
+    "Access-Control-Allow-Private-Network",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
